dcaf88b7401791154840ff08d5f4fbbd0fb7fb6b
Ascamel <sebastiankondraciuk3@gmail.com>
2022-10-15 10:44:39 +0200

    1st

--- Projekt2/MainWindow.xaml.cs
+++ Projekt2/MainWindow.xaml.cs
@@ -13,7 +13,9 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -29,10 +31,26 @@ namespace Projekt2
             InitializeComponent();
         }
 
-        public string values;
+        
 
         private void LoadButton_Click(object sender, RoutedEventArgs e)
         {
+            string values = "";
+            string line = "";
+
+            string Type;
+
+            int SizeX, SizeY, ColourSize;
+
+            PixelFormat pf = PixelFormats.Bgr24;
+
+            int width = 200;
+            int height = 200;
+            int rawStride = (width * pf.BitsPerPixel + 7) / 8;
+            byte[] rawImage = new byte[rawStride * height];
+
+            BitmapSource MyBitMap;//= BitmapSource.Create()
+
             OpenFileDialog dialog = new()
             {
                 Filter = "PPM File | *.ppm"
@@ -49,12 +67,49 @@ namespace Projekt2
 
 
 
-            using (StreamReader reader = new StreamReader(fs, Encoding.ASCII))
+            using (StreamReader reader = new(fs, Encoding.ASCII))
             {
-                values = reader.ReadToEnd();
-            }
-            Trace.WriteLine(values);
 
+                 values = reader.ReadToEnd();
+                
+                
+            }
+           // Trace.WriteLine(values);
+
+
+            foreach(char c in values)
+            {
+                if (c == 'P')
+                {
+                    if (values[values.IndexOf(c)+1].Equals('3'))
+                    {
+                        Type = "P3";
+                    }
+                    else
+                    {
+                        Type = "P6";
+                    }
+                }
+            }
+
+            MyBitMap = BitmapSource.Create(6, 6, 1, 1, pf, null, rawImage, rawStride);
+            MyImage.Source = MyBitMap;
+        }
+
+        public void AddImage()
+        {
+           // MyImage.Sou
+        }
+
+        public string ImageParser(string ToParse)
+        {
+            string result = "sad";
+            foreach(char c in ToParse)
+            {
+                Trace.WriteLine(c);
+            }
+
+            return result;
         }
     }
 }
